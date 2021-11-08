bb85a7830936cda6f8558071ec1116d6885867ae
NirTatcher <75395024+NirTatcher@users.noreply.github.com>
2021-11-09 00:35:52 +0200

    ...

--- src/components/Side.jsx
+++ src/components/Side.jsx
@@ -2,20 +2,21 @@ import React from 'react'
 import Card from './Card'
 export default function Side(props) {
 
-    return (
-        <div className="side">
-          
-        <div style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column' }}>
-          <h1 className="order-heading">רשימת הזמנות</h1>
-          <div className="num_of_orders_wrapper">
-            <h3 style={{ fontSize: '15px' }}>{props.persons.length}</h3>
-            <h3 style={{ fontSize: '15px' }} > :מספר הזמנות</h3>
-          </div>
-        </div>
-        {props.persons.map((person,index)=>
-            <Card key={index} editPerson={props.editPerson}  deletePerson={props.deletePerson} index={index} personDetails={{firstName:person.firstName,lastName:person.lastName,date:person.date}}/>
-        )}
+  return (
+    <div className="side">
 
+      <div style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column' }}>
+        <h1 className="order-heading">רשימת הזמנות</h1>
+        <div className="num_of_orders_wrapper">
+          <h3 style={{ fontSize: '15px' }}>{props.persons.length}</h3>
+          <h3 style={{ fontSize: '15px' }} > :מספר הזמנות</h3>
+        </div>
       </div>
-    )
+      <div className="cardsWrapper">
+        {props.persons.map((person, index) =>
+          <Card key={index} editPerson={props.editPerson} deletePerson={props.deletePerson} index={index} personDetails={{ firstName: person.firstName, lastName: person.lastName, date: person.date }} />
+        )}
+      </div>
+    </div>
+  )
 }
